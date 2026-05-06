8f7df433eb4d7c33379d4f3a5f33932d8b287d68
sukree <masjidtaram@gmail.com>
2026-05-06 08:15:17 +0700

    Restore and update from backup after HDD failure

---
+++ astro.config.ts
@@ -0,0 +1,94 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import { defineConfig } from 'astro/config';
+
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
+import icon from 'astro-icon';
+import compress from 'astro-compress';
+import type { AstroIntegration } from 'astro';
+
+import astrowind from './vendor/integration';
+
+import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin, lazyImagesRehypePlugin } from './src/utils/frontmatter';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const hasExternalScripts = false;
+const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroIntegration)[] = []) =>
+  hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
+
+export default defineConfig({
+  site: 'https://grainsure.co.th',
+  // ถ้าเป็นตอน build (production) ให้ใช้ 'always' เพื่อ SEO
+  // ถ้าเป็นตอนรันในเครื่อง (development) ให้ใช้ 'ignore' เพื่อให้รูปไม่พัง
+  trailingSlash: process.env.NODE_ENV === 'production' ? 'always' : 'ignore',
+  output: 'static',
+
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    mdx(),
+    icon({
+      include: {
+        tabler: ['*'],
+        'flat-color-icons': [
+          'template',
+          'gallery',
+          'approval',
+          'document',
+          'advertising',
+          'currency-exchange',
+          'voice-presentation',
+          'business-contact',
+          'database',
+        ],
+      },
+    }),
+
+    ...whenExternalScripts(() =>
+      partytown({
+        config: { forward: ['dataLayer.push'] },
+      })
+    ),
+
+    compress({
+      CSS: true,
+      HTML: {
+        'html-minifier-terser': {
+          removeAttributeQuotes: false,
+        },
+      },
+      Image: false,
+      JavaScript: true,
+      SVG: false,
+      Logger: 1,
+    }),
+
+    astrowind({
+      config: './src/config.yaml',
+    }),
+  ],
+
+  image: {
+    domains: ['cdn.pixabay.com'],
+  },
+
+  markdown: {
+    remarkPlugins: [readingTimeRemarkPlugin],
+    rehypePlugins: [responsiveTablesRehypePlugin, lazyImagesRehypePlugin],
+  },
+
+  vite: {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
+  },
+});
---
+++ src/navigation.ts
@@ -0,0 +1,131 @@
+import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  
+  links: [
+    {
+      text: 'หน้าหลัก',
+      href: getPermalink('//'),
+    },
+    {
+      text: 'ผลิตภัณฑ์',
+//      href: getPermalink('/#products'),
+      href: '/#products',
+    },
+    {
+      text: 'บริการ',
+      href: getPermalink('/services'),
+    },       
+    {
+      text: 'คลังความรู้',
+      href: getPermalink('/article'),
+    },       
+    {
+          text: 'เกี่ยวกับเรา',
+          href: getPermalink('/about-us'),
+    },
+
+  ],
+  // actions: [{ text: 'Download', href: 'https://github.com/arthelokyo/astrowind', target: '_blank' }],
+};
+
+export const footerData = {
+  links: [
+    {
+      title: 'สุดทางแล้ว ไปไหนต่อดี ?',
+      links: [
+        {
+          text: 'หน้าหลัก',
+          href: getPermalink('/'),
+        },
+        {
+          text: 'ผลิตภัณฑ์',
+          href: getPermalink('/') + '#products',
+        },
+        {
+          text: 'บริการ',
+          href: getPermalink('/services'),
+        },
+        {
+          text: 'เกี่ยวกับเรา',
+          href: getPermalink('/about-us'),
+        },
+        {
+          text: 'ทำไมต้องเลือกเรา',
+          href: getPermalink('/') + '#Our-Features',
+        },
+      ],
+    },
+    {
+      title: 'ผลิตภัณฑ์',
+      links: [
+        {
+          text: 'KU72-ข้าวเปลือก',
+          href: getPermalink('/Products/paddy-moisture-meter-ku72'),
+        },
+        {
+          text: 'KU72-ข้าวข้าวโพด',
+          href: getPermalink('/Products/corn-moisture-meter-ku72'),
+        },
+        {
+          text: 'KU60',
+          href: getPermalink('/Products/grain-moisture-meter-ku60'),
+        },
+        {
+          text: 'EE-KU',
+          href: getPermalink('/Products/grain-moisture-meter-eeku'),
+        },
+      ],
+    },    
+    {
+      title: 'บริการ',
+      links: [
+        {
+          text: 'ซ่อมเครื่องวัดความชื้น',
+          href: getPermalink('/services') + '#Fix-Repair',
+        },
+        {
+          text: 'ต่ออายุใบรับรอง',
+          href: getPermalink('/services') + '#cert-renewal',
+        },
+        {
+          text: 'รับผลิตตามสั่ง',
+          href: getPermalink('/services') + '#made-to-order',
+        },
+        {
+          text: 'เมล็ดที่รองรับ',
+          href: getPermalink('/services') + '#grain-features',
+        },
+      ],
+    },    
+    {
+      title: 'สิ่งน่าสนใจ',
+      links: [
+        {
+          text: 'คลังความรู้',
+          href: getPermalink('/article'),
+        },
+        {
+          text: 'โปรแกรมคำนวณความชื้น',
+          href: getPermalink('/') + '#gs-calculator',
+        },
+        {
+          text: 'คู่มือการใช้งาน',
+          href: getPermalink('/manual'),
+        },
+      ],
+    },    
+  ],
+  secondaryLinks: [
+    { text: 'Terms', href: getPermalink('') },
+    { text: 'Privacy Policy', href: getPermalink('') },
+  ],
+  socialLinks: [
+    { ariaLabel: 'Email', icon: 'tabler:mail', href: 'mailto:eeku.grainsure@gmail.com' },
+    { ariaLabel: 'Line', icon: 'tabler:brand-line', href: 'https://line.me/ti/p/~@grainsure#~' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/Grainsure/' },
+  ],
+  footNote: `
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/arthelokyo"> Arthelokyo</a> · All rights reserved.
+  `,
+};
